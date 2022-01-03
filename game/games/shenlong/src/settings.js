929c9c8f657520af33913945e962bd2386db0df0
UlyssesTakusen <bloodedplumeria@gmail.com>
2022-01-03 22:56:47 +0800

    Site updated: 2022-01-03 22:56:45

--- game/games/shenlong/src/settings.js
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>window._CCSettings = { platform: "web-mobile", groupList: ["default", "move", "UI", "fish"], collisionMatrix: [
-        [true, true, true, true],
-        [true, true, null, true],
-        [true, false, false],
-        [true, true, false, false]
-    ], hasResourcesBundle: true, hasStartSceneBundle: false, remoteBundles: [], subpackages: [], launchScene: "db://assets/Scene/MainGameScene.fire", orientation: "portrait", jsList: [], bundleVers: { internal: "803f8", resources: "93f29", main: "3786c" } };
